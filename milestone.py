1acc8aa0780ddc0f76eb23cd6fe8040cbde6ec8b
Shahar Shlomi <ShaharShlomi@gmail.com>
2021-09-12 14:35:19 -0400

    Still trying to get streamlit to work: removed all content from python script.

---
+++ milestone.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+import datetime
+import pandas_datareader.data as web
+from pandas import Series, DataFrame
+import streamlit as st
+
+st.title("Streamlit 101: An in-depth introduction")
