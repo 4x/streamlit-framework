e04698d9dd0362eaec90fda0917b9affe77a84bb
Shahar Shlomi <ShaharShlomi@gmail.com>
2021-09-13 12:07:01 -0400

    Adjusted close can be displayed. Symbol is CAPITALIZED on chart.

--- milestone.py
+++ milestone.py
@@ -8,7 +8,7 @@ from pandas import DataFrame
 from pandas_datareader import data as pdr
 import yfinance as yf
 from math import pi
-import calendar
+from calendar import monthrange
 import streamlit as st
 from bokeh.plotting import figure, show, output_file
 
@@ -22,12 +22,12 @@ st.title("Shahar's milestone project")
 with st.form("my_form"):
     symbol = st.text_input("Ticker?")
     date = st.date_input('Click month/year to scroll')
-    year = date.year
+    adjusted = st.checkbox("Display adjusted prices")
     month = date.month
     submitted = st.form_submit_button("Submit")
     if submitted:
-        start = datetime(year, month, 1)
-        end = datetime(year, month, calendar.monthrange(year, month)[1])
+        start = datetime(date.year, month, 1)
+        end = datetime(date.year, month, monthrange(date.year, month)[1])
         try:
             data = pdr.get_data_yahoo(symbol, start=start, end=end)
         except ValueError:
@@ -40,20 +40,13 @@ with st.form("my_form"):
                 print("Tried 5 times, Yahoo error. Trying after 2 minutes")
                 time.sleep(120)
                 data = pdr.get_data_yahoo(symbol, start=start, end=end)
-            
-            
-            
-        p = figure(x_axis_type="datetime", tools=TOOLS, title = symbol)
+                                    
+        p = figure(x_axis_type="datetime",
+                   tools=TOOLS, title = symbol.upper())
         p.xaxis.major_label_orientation = pi/4
         p.segment(data.index, data.High, data.index, data.Low, color="black")
-        p.vbar(data.index, w, data.Open, data.Close, fill_color="#D5E1DD", line_color="black")
+        if adjusted:
+            p.vbar(data.index, w, data.Open, data['Adj Close'], fill_color="#D5E1DD", line_color="black")
+        else:
+             p.vbar(data.index, w, data.Open, data.Close, fill_color="#D5E1DD", line_color="black")           
         st.write(p)
-
-#adjusted = st.checkbox("Display adjusted prices")
-
-
-# With a little help from https://stackoverflow.com/questions/43150211/python-bokeh-ohlc-plot-from-pandas-df
-
-
-#st.bokeh_chart(p) does not seem to work
-#st.write(p)
