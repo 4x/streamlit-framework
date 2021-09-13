1897c67188b079f02b3cf3454c9d393be388f4db
Shahar Shlomi <ShaharShlomi@gmail.com>
2021-09-13 09:51:20 -0400

    Added initial Bokeh OHLC plot

--- milestone.py
+++ milestone.py
@@ -9,10 +9,21 @@ import pandas as pd
 import datetime
 import pandas_datareader.data as web
 from pandas import Series, DataFrame
-import pandas_datareader as pdr
+from pandas_datareader import data as pdr
+import yfinance as yf
 
 import streamlit as st
 import fix_yahoo_finance
+from math import pi
+from bokeh.plotting import figure, show, output_file
+
+yf.pdr_override()
+data = pdr.get_data_yahoo(symbol, start="2017-01-01", end="2017-04-30")
+
+
+
+
+
 
 
 # Does NOT work
@@ -20,13 +31,24 @@ data = pdr.get_data_yahoo('NVDA')
 # Display Info
 print(data.info())
 
-import yfinance as yf
 # Request historical data for past 5 years
 symbol = "TSLA"
 data = yf.Ticker("NVDA").history(period='5y')
 data = yf.download(symbol, start="2017-01-01", end="2017-04-30")
 
+TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
+w = 12*60*60*1000/2000
 
+p = figure(x_axis_type="datetime", tools=TOOLS, plot_width=1000, title = "Candlestick")
+p.xaxis.major_label_orientation = pi/4
+
+p.segment(data.index, data.High, data.index, data.Low, color="black")
+p.vbar(data.index, w, data.Open, data.Close, fill_color="#D5E1DD", line_color="black")
+p.vbar(data.datetime[dec], w, data.open[dec], data.close, fill_color="#F2583E", line_color="black")
+p.vbar(data.datetime[inc], w, data.open[inc], data.close[inc], fill_color="#D5E1DD", line_color="black")
+p.vbar(data.datetime[dec], w, data.open[dec], data.close[dec], fill_color="#F2583E", line_color="black")
+
+show(p)
 
 
 
