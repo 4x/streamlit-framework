af86c7c5db9ccdc7809a485892d99dfe6653968d
Shahar Shlomi <ShaharShlomi@gmail.com>
2021-09-13 11:42:50 -0400

    Used Calendar to find last day of month

--- milestone.py
+++ milestone.py
@@ -3,39 +3,31 @@
 Data Incubator milestone project: stock chart
 """
 
-import datetime
+from datetime import datetime, timedelta
 from pandas import DataFrame
 from pandas_datareader import data as pdr
 import yfinance as yf
 from math import pi
-
+import calendar
 import streamlit as st
 from bokeh.plotting import figure, show, output_file
 
 yf.pdr_override()
-#symbol = "TSLA"
 
 TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
 w = 12*60*60*1000/2000
-#show(p)
 
 st.title("Shahar's milestone project")
 
 with st.form("my_form"):
     symbol = st.text_input("Ticker?")
-    date = st.date_input('Date input')
+    date = st.date_input('Click month/year to scroll')
     year = date.year
-    #last_day = datetime.datetime(year, month+1, 1)-1
-    month = st.number_input("Month", min_value=1, max_value=12)
-    year = st.number_input("Year", min_value=1990, value=2021)
-    #month = date.month
+    month = date.month
     submitted = st.form_submit_button("Submit")
     if submitted:
-        start = datetime.datetime(year, month, 1, 1)
-        end = datetime.datetime(year, month+1, 1)
-        
-        
-        
+        start = datetime(year, month, 1)
+        end = datetime(year, month, calendar.monthrange(year, month)[1])
         try:
             data = pdr.get_data_yahoo(symbol, start=start, end=end)
         except ValueError:
@@ -51,14 +43,11 @@ with st.form("my_form"):
             
             
             
-        #data = pdr.get_data_yahoo(symbol, start=start, end=end)
         p = figure(x_axis_type="datetime", tools=TOOLS, title = symbol)
         p.xaxis.major_label_orientation = pi/4
         p.segment(data.index, data.High, data.index, data.Low, color="black")
         p.vbar(data.index, w, data.Open, data.Close, fill_color="#D5E1DD", line_color="black")
-        st.bokeh_chart(p)
-        #show(p)
-
+        st.write(p)
 
 #adjusted = st.checkbox("Display adjusted prices")
 
